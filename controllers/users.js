015b1e2a0b307d41ca776e67f4577633d928ac68
Denis Shesternev <denshesternev@yandex.ru>
2023-07-28 10:21:49 +0800

    feat: 14 проектная, 1 иттерация

--- controllers/users.js
+++ controllers/users.js
@@ -48,10 +48,11 @@ const createUser = (req, res, next) => {
       },
     ))
     .catch((err) => {
+      if (err.code === 11000) {
+        throw new ConflictError('Пользователь с таким email уже существует');
+      }
       if (err.name === 'ValidationError') {
         throw new BadReqError('Переданы некорректные данные при создании пользователя');
-      } else if (err.code === 11000) {
-        throw new ConflictError('Пользователь с таким email уже существует');
       }
     })
     .catch(next);
