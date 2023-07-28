014296a8fa4071c5743d1dc4fc54a53d3fea7aea
Denis Shesternev <denshesternev@yandex.ru>
2023-07-28 19:56:24 +0800

    feat: 14 проектная, 1 иттерация

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const {
 
 const handelError = require('./middlewares/handleError');
 
-const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mesvxcvxcvx' } = process.env;
 
 const app = express();
 
