0d0b0d78d98fcb4a0d30a3bd6c1358ec38866ea5
Denis Shesternev <denshesternev@yandex.ru>
2023-07-29 14:09:06 +0800

    feat: 14 проектная, 2 иттерация

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const {
+  validationCreateUser,
+  validationLogin,
+} = require('./middlewares/validations');
 
 const handelError = require('./middlewares/handleError');
 
-const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mesvxcvxcvx' } = process.env;
+const { PORT = 3000, MONGODB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
 
 app.use(auth);
 app.use(routes);
